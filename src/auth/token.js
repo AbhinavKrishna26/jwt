496d51d944e443986e6fd828bd22517c0bd46bc3
Wiganstark18 <92356103+Wiganstark18@users.noreply.github.com>
2021-10-12 11:40:09 +0530

    [ADDED] Token.js

---
+++ src/auth/token.js
@@ -0,0 +1,12 @@
+-client
+   -app
+   -components
+    -all the libraries are here.
+   -index.html
+   -controllers.js
+  -node_modules
+  -server
+   -config
+    -server.js
+
+app.use(express.static(__dirname ));
